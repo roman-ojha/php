634813a112cf8d74ba13ef3c99c086b1574b577b
Roman-Ojha <razzroman98@gmail.com>
2022-11-02 21:35:11 +0545

    docs(tutorial): learned about variables

---
+++ Tutorial/Main_Tutorial/05_Variables/index.php
@@ -0,0 +1,21 @@
+<?php
+    // to declare variable name you have to use '$' at the start
+    $name="Roman";
+
+    // to access it
+    echo $name;
+    echo "<h1>".$name."</h1>";
+
+    $num=10.4;
+    echo $num;
+
+    // Right Way to create variable
+    $firstName;
+    $_firstName;
+    $first_name;
+    $firstName99;
+
+    // php is case sensitive language
+    $age;
+    $AGE;
+?>
