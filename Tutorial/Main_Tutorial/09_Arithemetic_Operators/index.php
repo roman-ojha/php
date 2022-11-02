b57af7076d169f986f94a31d7ebd84cb3ad87fb4
Roman-Ojha <razzroman98@gmail.com>
2022-11-02 23:04:10 +0545

    docs(tutorial): learned Arithemetic operators

---
+++ Tutorial/Main_Tutorial/09_Arithemetic_Operators/index.php
@@ -0,0 +1,49 @@
+<?php
+   /*
+        *) Arithmetic operator:
+            1) Addition => '+'
+            2) Subtraction => '-'
+            3) Multiplication => '*'
+            4) Exponentiation => '**'
+            5) Division => '/'
+            6) Modules (Remainder) => '%'
+            7) Increment Operator => '++'
+            8) Decrement Operator => '--'
+   */
+
+   $a=50;
+   $b=30;
+   $c=$a+$b;
+   echo $c,"<br/>";
+
+   $c=$a-$b;
+   echo $c,"<br/>";
+
+   $c=$a*$b;
+   echo $c,"<br/>";
+
+   $c=$a/$b;
+   echo $c,"<br/>";
+
+   $c=$a%$b;
+   echo $c,"<br/>";
+
+   $c=$a**$b;
+   echo $c,"<br/>";
+
+   $c=++$a;
+   echo $c,"<br/>";
+
+   $c=$a++;
+   echo $c,"<br/>";
+
+   $c=--$a;
+   echo $c,"<br/>";
+
+   $c=$a--;
+   echo $c,"<br/>";
+
+   $c=($a + $b) * 2;
+   echo $c,"<br/>";
+   
+?>
