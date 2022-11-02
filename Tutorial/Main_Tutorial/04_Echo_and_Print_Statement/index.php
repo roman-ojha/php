4b32868917c3e80ab1fd91517585aa0a7b547952
Roman-Ojha <razzroman98@gmail.com>
2022-11-02 21:26:34 +0545

    docs(tutorial): learned Echo and print statement

---
+++ Tutorial/Main_Tutorial/04_Echo_and_Print_Statement/index.php
@@ -0,0 +1,27 @@
+<?php
+// Echo statement
+    // -> can print multiple value
+    // -> echo is faster then print
+    echo "Hello world";
+    echo 'Hello world';
+
+    echo "Hello", "world";
+    // add multiple value
+
+    echo "<h1>Hello world</h1>";
+    // to print html tag
+
+    echo "<h2>Hello"."world</h2>";
+    // '.' use for concatenation
+
+    echo 502.25;
+    // print numeric value
+
+    echo ('Hello world');
+    // using ()
+
+// Print statement:
+    // -> can't print multiple value
+    print "Hello world";
+    print ("Hello world");
+?>
