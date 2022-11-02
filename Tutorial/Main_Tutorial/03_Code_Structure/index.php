365ca8945e44d5cb997b872acc69c06ed3e7a2cd
Roman-Ojha <razzroman98@gmail.com>
2022-11-02 21:08:08 +0545

    docs(tutorial): learned about code structure

---
+++ Tutorial/Main_Tutorial/03_Code_Structure/index.php
@@ -0,0 +1,32 @@
+<!-- 
+    *) PHP code syntax:
+        <?php
+            // Php code
+        ?>
+
+    *) PHP Code syntax in HTML Tags:
+        <html>
+            <head>
+                <title>index</title>
+            </head>
+            <body>
+                <?php
+                    // Php code
+                ?>
+            </body>
+        </html>
+ -->
+<?php
+    echo "Hello another";
+?>
+
+<html>
+    <head>
+        <title>index</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello from HTML syntax";
+        ?>
+    </body>
+</html>
