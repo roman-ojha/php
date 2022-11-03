0527a8bc3cd7ed3cb8767ec25c3043d0f87b4136
Roman-Ojha <razzroman98@gmail.com>
2022-11-03 21:11:56 +0545

    docs(tutorial): Termary & string operator

---
+++ Tutorial/Main_Tutorial/17_Ternary_Operator/index.php
@@ -0,0 +1,6 @@
+<?php
+   $a=15; 
+   ($a>10)?print("True"): print("False");
+
+   echo "Result is: " . ($a>10?"True": "False");
+?>
---
+++ Tutorial/Main_Tutorial/18_String_Operator/index.php
@@ -0,0 +1,6 @@
+<?php
+    $a="Hello";
+    // here we are reassigning to $a by using '.=' operator
+    $a .="World";
+    echo $a;
+?>
