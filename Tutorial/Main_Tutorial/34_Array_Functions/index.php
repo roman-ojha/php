2601a39ca1e4b8123e9bd07313dea646f021ebab
Roman-Ojha <razzroman98@gmail.com>
2022-11-04 22:20:21 +0545

    refactor: installed php cs fixer and code get refactored

--- Tutorial/Main_Tutorial/34_Array_Functions/index.php
+++ Tutorial/Main_Tutorial/34_Array_Functions/index.php
@@ -1,4 +1,5 @@
 <?php
+
     $name=["Roman","Razz","Harry","Jack","Tom","Roman",50];
     $emp = [
         [1,"Roman","Manager",800000],
@@ -10,17 +11,17 @@
         "physics"=>85,
         "chemistry"=>89,
         "math"=>78,
-       ], 
+       ],
        "Harry"=>[
         "physics"=>68,
         "chemistry"=>79,
         "math"=>73,
-       ], 
+       ],
        "Jack"=>[
         "physics"=>62,
         "chemistry"=>92,
         "math"=>67,
-       ], 
+       ],
     ];
 
     // Array Count: ===============================================
@@ -28,7 +29,7 @@
     // to get the size of array:
     echo count($name)."<br/>";
     // for multidimensional array
-    echo count($emp,1)."<br/>";
+    echo count($emp, 1)."<br/>";
     // to check size of internal multidimensional array
     echo count($emp[0])."<br/>";
 
@@ -44,29 +45,28 @@
     // Array Search: =================================================
     // in_array() ====
     // in_array(<value_to_search>,<array_variable_name>,<strict>);
-    echo "<br/>".in_array("Roman",$name);
+    echo "<br/>".in_array("Roman", $name);
     // return '1 | 0'
     // 1 when it found in array
     // p whe it didn't found in array
 
-    echo "<br/>".in_array("50",$name,true);
+    echo "<br/>".in_array("50", $name, true);
     // if 'strict' = false & we search for 55 and found '55' then return 1
     // if 'strict' = true & we search for 55 and found '55' then return 0
 
     // for multi dimensional array
-    echo "<br/>".in_array([1,"Roman","Manager",800000],$emp);
+    echo "<br/>".in_array([1,"Roman","Manager",800000], $emp);
 
     // array_search() ====
     // array_search(<value_to_search>,<array_variable_name>);
     // return 'index | key'
-    echo "<br/>".array_search("Roman",$name);
+    echo "<br/>".array_search("Roman", $name);
     // for multi dimensional array
     echo "<br/>".array_search(
         [
         "physics"=>85,
         "chemistry"=>89,
         "math"=>78,
-       ]
-        ,$marks);
-
-?>
+       ],
+        $marks
+    );
