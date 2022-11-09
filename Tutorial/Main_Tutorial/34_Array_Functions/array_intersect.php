e8f33d98a9ee9ae23be97e41ae33cede2fe00b3f
Roman-Ojha <razzroman98@gmail.com>
2022-11-09 15:07:11 +0545

    docs(tutorial): learned array_intersect with user defined function

--- Tutorial/Main_Tutorial/34_Array_Functions/array_intersect.php
+++ Tutorial/Main_Tutorial/34_Array_Functions/array_intersect.php
@@ -67,3 +67,80 @@
         [c] => Ojha
         )
     */
+
+    // array_intersect_uassoc(<array_1>,<array_2>,<user_defined_function>);
+    // u means user defined function
+    function compare($a, $b)
+    {
+        // return 0 when value get matched
+        // return 1 when '$a' value is greater then '$b' value
+        // return -1 when '$a' value is smaller then '$b' value
+
+        // now this function will get array as parameter
+        if ($a===$b) {
+            // if both of the value and keys get matched and it's types as well
+            return 0;
+        }
+        return ($a>$b) ? 1 : -1;
+    }
+    $newArray = array_intersect_uassoc($a, $b, "compare");
+    // here 'compare' is a function name
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
+    /*
+        Output:
+        Array
+        (
+            [a] => Roman
+            [c] => Ojha
+        )
+    */
+
+    // also we can use php string function
+    $newArray = array_intersect_uassoc($a, $b, "strcasecmp");
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
+
+
+    // array_uintersect_assoc(): same as 'array_intersect_uassoc()'
+    $newArray = array_uintersect_assoc($a, $b, "compare");
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
+
+    // array_intersect_ukey()
+    $newArray = array_intersect_ukey($a, $b, "compare");
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
+
+    // array_uintersect()
+    $newArray = array_uintersect($a, $b, "compare");
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
+
+    // array_uintersect_uassoc()
+    // this function will try to match keys as well as value but we have to create two different function
+    function compareKeys($a, $b)
+    {
+        // now this function will only compare and match keys
+        if ($a===$b) {
+            return 0;
+        }
+        return ($a>$b) ? 1 : -1;
+    }
+    function compareValue($a, $b)
+    {
+        // now this function will only compare and match values
+        if ($a===$b) {
+            return 0;
+        }
+        return ($a>$b) ? 1 : -1;
+    }
+    $newArray = array_uintersect_uassoc($a, $b, "compareKeys", "compareValue");
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
