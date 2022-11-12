0927ca9fd72db36573ace88d301b06830a73b86e
Roman-Ojha <razzroman98@gmail.com>
2022-11-12 15:23:32 +0545

    docs(tutorial): Intro to Magic Methods

---
+++ Tutorial/OOP/15_Magic_Methods/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    /*
+        *) Magic Methods
+            -> it automatically get called based on condition
+            -> for that we don't have to create object of that class
+            -> they starts with prefix(__)function_name
+            -> ex:
+                 -> '__construct()' is also the magic method
+            -> List:
+                01. __construct()
+                02. __destruct()
+                03. __get()
+                04. __set()
+                05. __isset()
+                06. __unset()
+                07. __autoload()
+                08. __clone()
+                09. __sleep()
+                10. __wakeup()
+                11. __call()
+                12. __callStatic()
+                13. __toString()
+                14. __invoke()
+    */
